55723c14f60f4364924ef222285d874a7f9de2f4
jackywangtj66 <63412857+jackywangtj66@users.noreply.github.com>
2022-03-29 13:17:55 +0800

    Update sparkx.py

--- spatialcv/models/sparkx.py
+++ spatialcv/models/sparkx.py
@@ -15,7 +15,6 @@ def sparkx(loc,count,X_mat = None):                           # loc: n × d 
         YTHS = count.transpose().dot(S)                    # each row is ytHS matrix (1*d)
         
         teststat = np.matrix(list(map(lambda x: YTHS[x,:].dot(STSinv).dot(YTHS[x,:].transpose())*YTYinv[0,x], range(p))))*n  
-        
         #teststat is n^2 times the teststat in paper, following the mixture chi dist.
         
         EE = list(map(lambda x: 1-n*YTYinv[0,x]*Ybar[0,x]**2,range(p)))         #eigenvalue of E, each element for each gene
@@ -23,17 +22,13 @@ def sparkx(loc,count,X_mat = None):                           # loc: n × d 
     else:                                                # X_mat  n*q matrix
         
         XTXinv = np.linalg.inv(X_mat.transpose().dot(X_mat))
-        
         adjust = X_mat.dot(XTXinv).dot(X_mat.transpose())
-        
         H = np.identity(n) - adjust
-        
         HS = H.dot(S)
         
         SigmaE = np.linalg.eig(STSinv.dot(HS.transpose()).dot(HS))[0]
         
         HY = H.dot(Y)
-        
         YTHY = HY.power(2).sum(axis = 1)
         
         YTHS = Y.transpose().dot(HS)
